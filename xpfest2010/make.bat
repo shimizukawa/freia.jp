3a40be00a8754212bc822aa7c4159eda30586750
Takayuki SHIMIZUKAWA <devnull@localhost>
2010-10-01 19:52:26 +0900

    * fix: iphone need input box width.

--- xpfest2010/make.bat
+++ xpfest2010/make.bat
@@ -3,7 +3,7 @@
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD=bin\sphinx-build
 )
 set BUILDDIR=build
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% source
